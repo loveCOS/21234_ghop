acb9e8398dfebb27799c43eda1a25a6b51ee4f6d
loveCOS <778693365@qq.com>
2019-09-09 22:26:15 +0800

    init gshop

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
+
+Vue.use(VueRouter)
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    {path:'/msite',component:MSite,
+    meta:{
+      showFooter:true
+    }},
+    {path:'/search',component:Search,
+    meta:{
+      showFooter:true
+    }},
+    {path:'/order',component:Order,
+    meta:{
+      showFooter:true
+    }},
+    {path:'/profile',component:Profile,
+    meta:{
+      showFooter:true
+    }},
+    {path:'/login',component:Login},
+    {path:'/',redirect:'/msite'}
+  ]
+})
+// export default new VueRouter({
+//   routes:[
+
+//   ]
+// })
